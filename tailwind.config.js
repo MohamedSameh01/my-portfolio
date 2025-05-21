d932afd715b43280977467a68e46cf8731ed3759
Mo sameh <ms7100100@gmail.com>
2025-05-21 23:18:03 +0300

    upload the test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayColor: "#F5F5F5",
+        brownColor: "#B68C31",
+        lightBrownColor: "#F5EEE0",
+        lightYelloColor: "#FFF7E8",
+      },
+    },
   },
   plugins: [],
 };
